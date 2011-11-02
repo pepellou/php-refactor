804d486c44808ae3207abc4aedc8ff8527a27ab4
Pepe Doval <pepellou@gmail.com>
2011-11-02 02:39:47 +0100

    Refactored tests

--- samples/duplicated-code/sample2/test/NumberTest.php
+++ samples/duplicated-code/sample2/test/NumberTest.php
@@ -4,32 +4,116 @@ include_once 'src/Number.php';
 
 class NumberTest extends PHPUnit_Framework_TestCase {
 
-	public function test_can_create() {
-		$this->assertEquals("123", Number::_new(123)->toString());
-		$this->assertEquals("1254", Number::_new(1254)->toString());
-		$this->assertEquals("10", Number::_new(10)->toString());
-		$this->assertEquals("0", Number::_new(0)->toString());
+	/**
+	 * @dataProvider create_data
+	 */
+	public function test_can_create(
+		$expected,
+		$number
+	) {
+		$this->assertEquals(
+			$expected, 
+			Number::_new($number)->toString()
+		);
 	}
 
-	public function test_can_clone() {
-		$num = new Number(123);
-		$num2 = $num->_clone();
-		$this->assertFalse($num == $num2);
-		$this->assertEquals($num->toString(), $num2->toString());
+	public function test_can_clone(
+	) {
+		$aNumber = new Number(123);
+		$cloned = $aNumber->_clone();
+		$this->assertFalse($aNumber == $cloned);
+		$this->assertEquals($aNumber->toString(), $cloned->toString());
 	}
 
-	public function test_can_sum() {
-		$this->assertEquals("50", Number::_new(15)->sum(new Number(35))->toString());
+	/**
+	 * @dataProvider sum_data
+	 */
+	public function test_can_sum(
+		$expected,
+		$term1,
+		$term2
+	) {
+		$this->assertEquals(
+			$expected, 
+			Number::_new($term1)->sum(new Number($term2))->toString()
+		);
 	}
 
-	public function test_can_multiply() {
-		$this->assertEquals("525", Number::_new(15)->multiply(new Number(35))->toString());
+	/**
+	 * @dataProvider multiply_data
+	 */
+	public function test_can_multiply(
+		$expected,
+		$factor1,
+		$factor2
+	) {
+		$this->assertEquals(
+			$expected, 
+			Number::_new($factor1)->multiply(new Number($factor2))->toString()
+		);
 	}
 
-	public function test_can_power() {
-		$this->assertEquals("225", Number::_new(15)->power(2)->toString());
-		$this->assertEquals("625", Number::_new(5)->power(4)->toString());
+	/**
+	 * @dataProvider power_data
+	 */
+	public function test_can_power(
+		$expected,
+		$base,
+		$power
+	) {
+		$this->assertEquals(
+			$expected, 
+			Number::_new($base)->power($power)->toString()
+		);
 	}
+
+	public function create_data(
+	) {
+		return array(
+			"case 1" => array("123",  123),
+			"case 2" => array("1254", 1254),
+			"case 3" => array("10",   10),
+			"case 4" => array("0",    0)
+		);
+	}
+
+	public function sum_data(
+	) {
+		return array(
+			"case 1" => array("50",  15,  35),
+			"case 2" => array("576", 159, 417),
+			"case 3" => array("320", 156, 164),
+			"case 4" => array("42",  15,  27),
+			"case 5" => array("27",  0,   27),
+			"case 6" => array("27",  27,  0),
+			"case 7" => array("0",   0,   0)
+		);
+	}
+
+	public function multiply_data(
+	) {
+		return array(
+			"case 1" => array("525", 15,   35),
+			"case 2" => array("225", 15,   15),
+			"case 3" => array("576", 48,   12),
+			"case 4" => array("0",   1578, 0),
+			"case 5" => array("0",   0,    2349),
+			"case 6" => array("123", 123,  1),
+			"case 7" => array("123", 1,    123)
+		);
+	}
+
+	public function power_data(
+	) {
+		return array(
+			"case 1" => array("225",     15,  2),
+			"case 2" => array("625",     5,   4),
+			"case 3" => array("1048576", 2,   20),
+			"case 4" => array("1",       232, 0),
+			"case 5" => array("0",       0,   12)
+		);
+	}
+
 }
 
 ?>
